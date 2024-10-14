85e44a1cb4c9ce612f77945db6ed824bdab4ca4b
Shwetagithub24 <shwetazarekar6@gmail.com>
2024-10-14 12:18:27 +0530

    Setup and requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+# describe module distribution
+
+from setuptools import find_packages,setup
+from typing import List
+
+#HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)-> List[str]:
+   '''It will return the list of requirements'''
+   requirements=[]
+
+   with open(file_path) as file_obj:
+      requirements=file_obj.readlines()
+      requirements= [req.replace("/n","") for req in requirements]
+
+      #if HYPEN_E_DOT in requirements:
+         #requirements.remove(HYPEN_E_DOT)
+
+   return requirements
+
+setup(
+name="predictive_maintenance-_system",
+version="0.0.1",
+author="shwetanjali",
+author_email="shwetazarekar6@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
+
