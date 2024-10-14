da36a81dfa7e86a77cd9e6663557992b1810351d
Shwetagithub24 <shwetazarekar6@gmail.com>
2024-10-14 16:27:56 +0530

    Logging and Exception

---
+++ app.py
@@ -0,0 +1,7 @@
+from src.predictive_maintenance_system.logger import logging
+from src.predictive_maintenance_system.exception import CustomException
+import sys
+
+if __name__=="__main__":
+    logging.info("The execution has started.")
+
